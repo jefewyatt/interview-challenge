e3827e531f6320088b805038a3c9cf529d2adabe
jefewyatt <jefewyatt@gmail.com>
2015-12-15 18:17:46 -0800

    Adding comments

--- src/main/java/com/intuit/ctg/qa/a4a/Student.java
+++ src/main/java/com/intuit/ctg/qa/a4a/Student.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
+/**
+ * INSTRUCTIONS
+ * Please write unit tests for this class in StudentTest.java
+ * 
+ * Comment on and fix any issues your unit tests find in Student.java
+ */
 public class Student {
     
     private String name;
@@ -11,38 +17,52 @@ public class Student {
     private double gpa;
     private static ArrayList<String> courses;
     
+    /**
+     * Instantiates a Student object
+     */
     public Student(String name, Date date, Double gpa) {
         this.name = name;
         this.birthdate = date;
         this.gpa = gpa;
     }
     
+    /**
+     * Adds a course to the student's list of courses
+     */
     public void addCourse(String courseName) {
         courses.add(courseName);
     }
     
+    /**
+     * Returns true if a student is taking a course, false otherwise
+     */
     public boolean hasCourse(String courseName) {
         return courses.contains(courseName);
     }
     
+    /* Returns the students name */
     public String getName() {
         return name;
     }
     
+    /* Returns the student's name with the last name, followed by the first name.
+     * For example: "John Smith" should return "Smith, John"
+     */
     public String getLastNameFirst() {
         String[] names = name.split(" ");
         return names[1] + ", " + names[0];
     }
     
+    /* Returns true if today is student's birthday, false otherwise */
     public boolean isBirthday(Date today) {
         Calendar now = Calendar.getInstance();
-        now.setTime(today);
         Calendar dob = Calendar.getInstance();
         dob.setTime(birthdate);
         
         return now.get(Calendar.DAY_OF_YEAR) == dob.get(Calendar.DAY_OF_YEAR);
     }
     
+    /* Returns a student object representing the student with a higher GPA */
     public Student getBetterStudent(Student otherStudent) {
         if (this.gpa > otherStudent.gpa) {
             return this;
@@ -50,6 +70,7 @@ public class Student {
         return otherStudent;
     }
     
+    /* Returns true if the the student has the same name, birthday, and GPA as this student. False otherwise */
     @Override
     public boolean equals(Object other) {
         Student otherStudent = (Student)other;
